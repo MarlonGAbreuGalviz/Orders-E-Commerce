75187302005e20627a64d03a92894fa0903dbc7c
MarlonGAbreuGalviz <marlonabreu10@gmail.com>
2026-04-28 10:33:54 -0400

    First commit

---
+++ orders/src/test/java/cl/fullstack/orders/OrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.fullstack.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
